41c9ba616deb818c869aec92e6ab82fa95a9a828
Alistair Willis <alistairrwillis@gmail.com>
2020-08-10 10:14:08 +0100

    add app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Menu, MenuItemProps } from "semantic-ui-react";
+import BasicForm from "./components/BasicForm";
+import Formik from "./components/Formik";
+import Hooks from "./components/Hooks";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface MenuArgs {
+  active: boolean;
+  name: string;
+  onClick: () => {};
+}
+
+class App extends Component {
+  state = {
+    activeItem: "Basic Form",
+  };
+
+  handleItemClick = (data: MenuItemProps) => {
+    console.log(data);
+    this.setState({ activeItem: data.name });
+  };
+
+  render() {
+    const { activeItem } = this.state;
+    return (
+      <div>
+        <Menu attached="top" tabular>
+          <Menu.Item
+            name="Basic Form"
+            active={activeItem === "Basic Form"}
+            onClick={(e, data) => this.handleItemClick(data)}
+          />
+          <Menu.Item
+            name="Formik"
+            active={activeItem === "Formik"}
+            onClick={(e, data) => this.handleItemClick(data)}
+          />
+
+          <Menu.Item
+            name="Hooks"
+            active={activeItem === "Hooks"}
+            onClick={(e, data) => this.handleItemClick(data)}
+          />
+        </Menu>
+
+        <div className="content-container">
+          {activeItem === "Basic Form" && <BasicForm />}
+          {activeItem === "Formik" && <Formik />}
+          {activeItem === "Hooks" && <Hooks />}
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Formik.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Formik extends Component {
+    render() {
+        return (
+            <div>Formik</div>
+        )
+    }
+}
+
+export default Formik;
